ca04d7e3f3bb84a9183c6541a490d50ed4f0ecd6
Uliana Melevych <uljamelevich17@gmail.com>
2025-03-22 00:17:38 +0200

    add file

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,10 @@
 const form = document.querySelector(".feedback-form");
 const STORAGE_KEY = "feedback-form-state";
 
-// Об'єкт для збереження даних форми
+
 let formData = { email: "", message: "" };
 
-// Перевірка локального сховища при завантаженні сторінки
+
 document.addEventListener("DOMContentLoaded", () => {
     const savedData = localStorage.getItem(STORAGE_KEY);
     if (savedData) {
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-// Відстеження введення даних та їх збереження у локальне сховище
+
 form.addEventListener("input", (event) => {
     formData[event.target.name] = event.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
 
-// Обробка події відправлення форми
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -31,7 +31,7 @@ form.addEventListener("submit", (event) => {
 
     console.log("Submitted data:", formData);
 
-    // Очищення форми, локального сховища та об'єкта formData
+    
     localStorage.removeItem(STORAGE_KEY);
     formData = { email: "", message: "" };
     form.reset();
